1ad03dc8bc536920693b867af51a07f206aff6a7
Alfie Chadwick <75963032+Fonzzy1@users.noreply.github.com>
2021-10-13 20:55:52 +1100

    Initial Window content

--- assets/js/content.js
+++ assets/js/content.js
@@ -16,9 +16,18 @@ chrome.runtime.onMessage.addListener( function (request) {
             // Underline and turn text green for each paragraph
             ps[p_id].style.backgroundColor = 'green'
 
-            ps[p_id].onclick = function () {
-                alert(displayArray[i].message)
-            };
+            ps[p_id].onmouseover = function (e) {
+                popupWindow =window.open("", "Climate Genie", "toolbar=yes,scrollbars=no,resizable=no,menubar=no,titlebar=no,top="+ (e.pageY)+ ",left="+ (e.pageX)+ ",width=400,height=400");
+		popupWindow.document.write('<html><head> <title>Climate Genie</title><link rel="stylesheet" type="text/css" href="css/newsCSSWindow.css"></head><body>');
+                popupWindow.document.write(displayArray[i].message);
+                popupWindow.document.write('</body></html>');;
+            }
+            
+            ps[p_id].onmouseleave = function () {
+		 popupWindow.close()
+            }
+            
+            ;
         }
     }
     return true
