1541901ea53bcde5731e1119978142300f59621a
KalykeXIII <kyleherbertson4@gmail.com>
2021-10-13 21:41:23 +1100

    Popup cleaning/docs as well as colour changes for the text highlighting

--- assets/js/content.js
+++ assets/js/content.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener( function (request) {
     // If the received message is for the display function
 
     if (request.message == 'disp') {
-        // Retrive the display array from the mesae
+        // Retrive the display array from the message array
         var displayArray = request.content
 
         // Iterate function over length of array, note all cat 0.0 has been previously removed
@@ -14,20 +14,21 @@ chrome.runtime.onMessage.addListener( function (request) {
             var ps = document.getElementsByTagName('p')
 
             // Underline and turn text green for each paragraph
-            ps[p_id].style.backgroundColor = 'green'
+            ps[p_id].style.backgroundColor = "rgba(60, 179, 113, 0.5)"
 
+            // When hovering over the highlighted element bring up a popup containing the debunk message
             ps[p_id].onmouseover = function (e) {
-                popupWindow =window.open("", "Climate Genie", "toolbar=yes,scrollbars=no,resizable=no,menubar=no,titlebar=no,top="+ (e.pageY)+ ",left="+ (e.pageX)+ ",width=400,height=400");
-		popupWindow.document.write('<html><head> <title>Climate Genie</title><link rel="stylesheet" type="text/css" href="css/newsCSSWindow.css"></head><body>');
+                // Create a new nonresizable window called 'Climate Genie'
+                popupWindow = window.open("", "Climate Genie", "toolbar=yes,scrollbars=no,resizable=no,menubar=no,titlebar=no,location=no,top="+ (e.pageY)+ ",left="+ (e.pageX)+ ",width=400,height=400");
+		        popupWindow.document.write('<html><head> <title>Climate Genie</title></head><body>');
                 popupWindow.document.write(displayArray[i].message);
                 popupWindow.document.write('</body></html>');;
-            }
+            };
             
+            // On leaving the text object close any open popups
             ps[p_id].onmouseleave = function () {
-		 popupWindow.close()
-            }
-            
-            ;
+                popupWindow.close()
+            };
         }
     }
     return true
